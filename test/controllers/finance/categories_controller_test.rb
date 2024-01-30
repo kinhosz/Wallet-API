b77460669433fbbfbb8f68be585fc060d5e3eb8f
kinhosz <jcsc@odoo.com>
2024-01-30 20:48:15 +0100

    test: add test for Finance::Category

--- test/controllers/finance/categories_controller_test.rb
+++ test/controllers/finance/categories_controller_test.rb
@@ -1,7 +1,60 @@
 require "test_helper"
 
 class Finance::CategoriesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    user = users(:one)
+    sign_in user
+  end
+
+  teardown do
+    sign_out :user
+  end
+
+  test "should not create category when not logged in" do
+    sign_out :user
+    post finance_categories_url, params: {
+      category: {
+        name: "credit card",
+        description: "credit card category",
+        category_type: "expense",
+        is_recurring: true
+      }
+    }
+
+    assert_response :unauthorized
+  end
+
+  test "should create a category" do
+    post(
+      finance_categories_url,
+      params: {
+        "category": {
+          "name": "credit card",
+          "description": "credit card category",
+          "category_type": "expense",
+          "is_recurring": true
+        }
+      }
+    )
+
+    assert_response :success
+  end
+
+  test "should return unprocessable_entity" do
+    post(
+      finance_categories_url,
+      params: {
+        "category": {
+          "name": "",
+          "description": "",
+          "category_type": "",
+          "is_recurring": false
+        }
+      }
+    )
+
+    assert_response :unprocessable_entity
+  end
 end
