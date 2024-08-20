a4a31833cad64a89dbc0410b21b8eaec536f94cc
kinhosz <jcsc@odoo.com>
2024-08-20 13:35:42 +0200

    [FEAT] action: add planning/current

--- app/controllers/finance/plannings_controller.rb
+++ app/controllers/finance/plannings_controller.rb
@@ -1,5 +1,6 @@
 class Finance::PlanningsController < ApplicationController
   before_action :authenticate_user!
+  before_action :ensure_currency_present, only: [:current]
   respond_to :json
 
   def create
@@ -38,8 +39,23 @@ class Finance::PlanningsController < ApplicationController
     end
   end
 
+  def current
+    planning = current_user.finance_plannings(params[:currency]).last
+    categories = current_user.finance_categories
+
+    balance = BalanceResume.new(categories, planning.date_start, planning.date_end, params[:currency]).get
+    computed = PlanningCalculator.new(planning).compute
+    render json: computed.merge(balance)
+  end
+
   private
   def finance_planning_params
     params.require(:planning).permit(:currency, :date_start, :date_end, lines: [:category, :value])
-  end  
+  end
+
+  def ensure_currency_present
+    unless params[:currency].present?
+      render json: { error: "Currency parameter is required" }, status: :bad_request
+    end
+  end
 end
