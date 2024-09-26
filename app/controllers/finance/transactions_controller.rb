2a43b5399d724d50a0060f1e6298c4a9dc1e2259
laysearaujo <lrsa@cin.ufpe.br>
2024-09-26 15:01:23 -0300

    feat: get transactions by date

--- app/controllers/finance/transactions_controller.rb
+++ app/controllers/finance/transactions_controller.rb
@@ -5,6 +5,27 @@ class Finance::TransactionsController < ApplicationController
   def index
     transactions = Finance::Transaction.joins(:finance_category)
                                        .where(finance_categories: { user_id: current_user.id })
+
+    render json: Finance::TransactionSerializer.new(transactions).serializable_hash[:data].map { |transaction| transaction[:attributes] }
+  end
+
+  def index_by_date
+    puts "Start Date: #{params[:start_date]}"
+    puts "End Date: #{params[:end_date]}"
+    transactions = Finance::Transaction.joins(:finance_category)
+                                       .where(finance_categories: { user_id: current_user.id })
+  
+    if params[:start_date].present? && params[:end_date].present?
+      # Filtro para intervalo de datas
+      transactions = transactions.where(occurred_at: params[:start_date]..params[:end_date])
+    elsif params[:start_date].present?
+      # Filtro para transações que ocorreram exatamente na data de start_date
+      transactions = transactions.where(occurred_at: params[:start_date])
+    elsif params[:end_date].present?
+      # Filtro para transações que ocorreram até a data de end_date
+      transactions = transactions.where('occurred_at <= ?', params[:end_date])
+    end
+  
     render json: Finance::TransactionSerializer.new(transactions).serializable_hash[:data].map { |transaction| transaction[:attributes] }
   end
 
@@ -45,6 +66,8 @@ class Finance::TransactionsController < ApplicationController
   private
 
   def transaction_params
-    params.require(:transaction).permit(:occurred_at, :description, :value, :currency, :category)
+    params.require(:transaction).permit(
+      :occurred_at, :description, :value, :category, :currency
+    )
   end
 end
