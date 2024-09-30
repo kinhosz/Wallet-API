a5fa5ab26a35becc298282c5b1f0ae337d5a2696
kinhosz <jcsc@odoo.com>
2024-09-30 22:38:23 +0200

    [DB] categories: add icon column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_20_104843) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_30_195232) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_20_104843) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.uuid "uuid", default: -> { "gen_random_uuid()" }
+    t.integer "icon", default: -1
     t.index ["user_id"], name: "index_finance_categories_on_user_id"
   end
 
