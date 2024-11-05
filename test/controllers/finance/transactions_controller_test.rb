e267318ec086dc7a62bb34513165dfba236465c2
laysearaujo <lrsa@cin.ufpe.br>
2024-11-05 12:06:53 -0300

    feat: add test

--- test/controllers/finance/transactions_controller_test.rb
+++ test/controllers/finance/transactions_controller_test.rb
@@ -52,7 +52,7 @@ class Finance::TransactionsControllerTest < ActionDispatch::IntegrationTest
         
         assert_equal @transaction[:transaction][:description],
             json_response['description']
-        
+
         assert_equal @transaction[:transaction][:occurred_at].strftime('%Y-%m-%d'),
             json_response['occurred_at']
         
@@ -67,5 +67,45 @@ class Finance::TransactionsControllerTest < ActionDispatch::IntegrationTest
         
         json_response = JSON.parse(response.body)
         assert_includes json_response.keys, "category"
+    end
+
+    # ------------------------------
+    # GET #index
+    # ------------------------------
+
+    test "should get index" do
+        get finance_transactions_url
+    
+        assert_response :success
+    
+        json_response = JSON.parse(response.body)
+        assert json_response.is_a?(Array)
+        assert_not json_response.empty?
+    
+        # Verifica se a transação retornada está correta
+        assert_equal @transaction.description, json_response[0]['description']
+        assert_equal @transaction.occurred_at.strftime('%Y-%m-%d'), json_response[0]['occurred_at']
+        assert_equal @transaction.value.to_f, json_response[0]['value']
+      end
+
+    
+    # ------------------------------
+    # GET #index_by_date
+    # ------------------------------
+
+    test "should get index_by_date with specific date" do
+    specific_date = Date.today.strftime('%Y-%m-%d')
+
+    get filter_by_date_finance_transactions_url, params: { start_date: specific_date }
+
+    assert_response :success
+
+    json_response = JSON.parse(response.body)
+    assert json_response.is_a?(Array)
+    
+    # Verifica que a resposta só contém transações na data específica
+    json_response.each do |transaction|
+        assert_equal specific_date, transaction['occurred_at']
+    end
     end
 end
